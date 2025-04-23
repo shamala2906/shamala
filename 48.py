18a5bfb52528679d87ff04879d3c32aae6dc19a1
shamala2906 <pshamala104@gmail.com>
2025-04-23 09:43:22 +0530

    Add files via upload

---
+++ 48.py
@@ -0,0 +1,45 @@
+
+#Define a functi
+'personal_details'.
+def personal_details():
+    #Define variables 'name' and 'age' and assign values to them.
+    name,age="Simon",19
+    #Define a variable 'address' and assign a value to it.
+       address="Bangalore,Karnataka,India"
+       #Print the personal details using string formatting.
+       print("Name:{}\nAge:{}\nAddress:{}".format(name,age,address))
+       #Call the 'personal_details' function to display the details.
+       personal_details()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
